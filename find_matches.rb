16ee8b6fa192e6c50836e0d53f9179f5311e1d2b
Thomas R. Koll <tomk32@tomk32.de>
2009-07-30 03:03:16 +0200

    totally cutting it down

--- find_matches.rb
+++ find_matches.rb
@@ -26,24 +26,25 @@ tests.split("\n").each do |uid|
     users[compid].each do |repoid|
       common[repoid] ||= 0
       # higher score if the users share a lot of projects
-      common[repoid] += usermap[uid].size / ((usermap[uid] & usermap[compid]).size + 1)
+      common[repoid] += (users[compid].size / usermap[uid].size) * ((usermap[uid] & usermap[compid]).size + 1)
       next if project_lang_map[repoid].nil? or user_lang_map[uid].nil?
     end
   end
   project_lang_map.each do |repoid, languages|
     common[repoid] ||= 0
-    languages.each_pair do |language, lines|
-      begin
-        next if user_lang_map[uid].empty? or user_lang_map[uid][language].nil?
-      rescue Exception => ex
-        puts 'user: ', uid
-        puts 'user languages:', user_lang_map[uid].inspect
-        puts 'language:', language.inspect
-        exit(0)
-      end
-      # high score if the repo and user share lots of LOC and the user doesn't watch many repos
-      common[repoid] += usermap[uid].size / ((lines.to_i) / (user_lang_map[uid][language].to_i + 1) + 1)
-    end
+    common[repoid] += (user_lang_map[uid].keys & languages.keys).size
+#    languages.each_pair do |language, lines|
+#      begin
+#        next if user_lang_map[uid].empty? or user_lang_map[uid][language].nil?
+#      rescue Exception => ex
+#        puts 'user: ', uid
+#        puts 'user languages:', user_lang_map[uid].inspect
+#        puts 'language:', language.inspect
+#        exit(0)
+#      end
+#      # high score if the repo and user share lots of LOC and the user doesn't watch many repos
+#      common[repoid] += usermap[uid].size / ((lines.to_i) / (user_lang_map[uid][language].to_i + 1) + 1)
+#    end
   end
   friends = common.sort { |a, b| a[1] <=> b[1] }.reverse[0, 10]
   friends = friends.map { |a| a[0] }
